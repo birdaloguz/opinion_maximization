9f34448cb21d25cb026d1b7d1087aa9ecb9949ad
birdaloguz <birdaloguz@gmail.com>
2020-02-25 15:08:11 +0100

    setup

--- cone.py
+++ cone.py
@@ -6,12 +6,12 @@ import networkx as nx
 ##get matrix
 """movie_ratings = pd.read_csv('CiaoDVD/movie-ratings.txt', names=["user_id", "movie_id", "genre_id", "review_id", "rating", "timestamp"],
                 header=None, sep=',', engine='python').drop_duplicates(subset=['user_id', 'movie_id'])"""
-movie_ratings = pd.read_csv('random_dataset/m_dataset.txt', names=["user_id", "movie_id", "rating"],
-                header=None, sep=':', engine='python')
+movie_ratings = pd.read_csv('random_dataset/dataset.csv', names=["user_id", "item_id", "rating"],
+                header=None, sep=',', engine='python')
 
 #average rating for opinion matrix
 r_avg = movie_ratings['rating'].mean()
-matrix_um = movie_ratings.pivot(index='user_id', columns='movie_id', values='rating').fillna(0)
+matrix_um = movie_ratings.pivot(index='user_id', columns='item_id', values='rating').fillna(0)
 
 #drop users with less than 5 ratings
 droplist = [i for i in matrix_um.columns if np.count_nonzero(matrix_um[i])<5]
@@ -21,18 +21,12 @@ matrix_um.drop(droplist, axis=1, inplace=True)
 A = matrix_um.values
 
 ##load graph
+
 G = nx.DiGraph()
-"""with open('CiaoDVD/trusts.txt') as fp:
+with open('random_dataset/dataset_trust.csv') as fp:
     for idx, line in enumerate(fp.readlines()):
-        a = line.split(',')[1]
-        b = line.split(',')[0]
-        if int(a) <= 17615 and int(b) <= 17615:
-            G.add_edge(int(a), int(b))"""
-G = nx.DiGraph()
-with open('random_dataset/trusts.txt') as fp:
-    for idx, line in enumerate(fp.readlines()):
-        a = line.split(':')[0]
-        b = line.split(':')[1]
+        a = line.split(',')[0]
+        b = line.split(',')[1]
         G.add_edge(int(a), int(b))
 
 for edge in list(G.edges):
@@ -48,8 +42,7 @@ for edge in list(G.edges):
 
 for node in list(G.nodes):
     G.nodes[node]['threshold'] = round(random.uniform(0, 1), 3)
-
-
+print(-1)
 #initialization -- fill matrix
 R_tilda, opinion_matrix, U, V, val, val_ratings= get_rating_estimations(A, validation=True)
 #delete validation item
@@ -61,17 +54,22 @@ V = np.append(V, v_t, axis=0)
 
 
 C_p = [] #activated nodes
-T = 20 #rounds
+T = 50 #rounds
 seed_set = []
 opinion_set = []
 
+import time
 for t in range(T):
+    print(t)
+    start = time.time()
     v_t = V[-1] #new product profile
     r = np.dot(U, v_t.T) #new product ratings
 
     r_q = r - r_avg #new product opinions
     R_new_p = np.dot(U, V.T) #ratings matrix
+
     S_k, C_p, O_max = greedy_om(G, r_q, C_p=C_p)
+
     seed_set.append(S_k)
     opinion_set.append(O_max)
     C_p.sort()
@@ -83,12 +81,14 @@ for t in range(T):
             r_v[idx]=0
 
     updated_A = np.hstack((A, np.reshape(r_v, (len(r_v), 1))))
-
     R_tilda, opinion_matrix, U, V = get_rating_estimations(updated_A)
-
+    elapsed = start - time.time()
+    print(elapsed)
 
 print(seed_set)
 print(val)
+print(list(val_ratings))
+print(r_avg)
 print(C_p)
 np.savetxt("val_ratings.txt", val_ratings, newline=" ")
 
--- sgd.py
+++ sgd.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import random
 
-# Scoring Function: Root Mean Squared Error
+#RMSE
 def rmse_score(R, Q, P):
     I = R != 0  # Indicator function which is zero for missing data
     ME = I * (R - np.dot(P, Q.T))  # Errors between real and predicted ratings
@@ -15,7 +15,7 @@ def get_rating_estimations(R, validation=False):
     f = 3  # Number of latent factor pairs
     lmbda = 0.50  # Regularisation strength
     gamma = 0.01  # Learning rate
-    n_epochs = 15  # Number of loops through training data
+    n_epochs = 6  # Number of loops through training data
     U = 3 * np.random.rand(n_u, f)  # Latent factors for users
     V = 3 * np.random.rand(n_m, f)  # Latent factors for movies
 
@@ -26,21 +26,19 @@ def get_rating_estimations(R, validation=False):
             U[u, :] += gamma * (e * V[i, :] - lmbda * U[u, :])  # Update this user's features
             V[i, :] += gamma * (e * U[u, :] - lmbda * V[i, :])  # Update this movie's features
 
+    R_tilda = np.dot(U, V.T)
 
     if validation:
         val = random.randint(0, 400)
         val_v = V[val]
         V = np.delete(V, np.s_[val], axis=0)
-        R_tilda = np.dot(U, V.T)
+
         r_avg = np.true_divide(R.sum(0), (R != 0).sum(0)).mean()
         opinion_matrix = R_tilda - r_avg
         val_ratings = np.dot(U, val_v.T)
         return R_tilda, opinion_matrix, U, V, val, val_ratings
 
-
-    R_tilda = np.dot(U, V.T)
     r_avg = np.true_divide(R.sum(0), (R != 0).sum(0)).mean()
     opinion_matrix = R_tilda - r_avg
-
 
     return R_tilda, opinion_matrix, U, V
